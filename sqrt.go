e243aaa27ae5e64e4553636187702b4b6998282f
test <test@tester.com>
2017-03-25 01:04:17 +0900

    hoge

--- sqrt.go
+++ sqrt.go
@@ -1,13 +1,47 @@
-// This package is a Travis-CI example package.
-package newmath
+package main
 
-// Sqrt returns an approximation to the square root of x.
+import (
+	"fmt"
+)
+
+// Inte is greatest interface
+type Inte interface {
+	afbbw() int
+}
+
+// Again is not stricts
+type Again struct {
+	a, x int
+	y, g string
+}
+
+// Test tests
+func (a Again) Test(x int) int {
+	return a.a
+}
+
+func (a Again) afbbw() int {
+	return 5
+}
+
+// Sqrt goes wrong
 func Sqrt(x float64) float64 {
-	// This is a terrible implementation.
-	// Real code should import "math" and use math.Sqrt.
-	z := 0.0
+	z := 0.3
 	for i := 0; i < 1000; i++ {
 		z -= (z*z - x) / (2 * x)
 	}
 	return z
+}
+
+func main() {
+	var a Inte
+
+	fmt.Println("fwefw")
+	aga := Again{y: "foiew", a: 32, x: 54, g: "str"}
+	b := aga.Test(3)
+	a = aga
+	if a != nil {
+
+	}
+	fmt.Print(b)
 }
---
+++ test.go
@@ -0,0 +1,7 @@
+package main
+
+func a() int {
+	g := 32
+	g = g + 1
+	return 0
+}
